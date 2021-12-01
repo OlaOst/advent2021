a4912b2eadc198dcf788f62dc72cfdb593b7fc08
Ola <ola@superoffice.com>
2021-12-01 13:38:37 +0100

    Day 1 part 1

--- source/app.d
+++ source/app.d
@@ -1,6 +1,18 @@
-import std.stdio;
+import std;
+
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	"day1input.txt".File.byLine.map!(line => line.to!int).array.day1part1.writeln;
 }
+
+unittest
+{
+  auto heights = [199,200,208,210,200,207,240,269,260,263];
+  assert(heights.day1part1 == 7);
+}
+auto day1part1(int[] input)
+{
+  return input.zip(input[1..$]).count!(i => i[0] < i[1]);
+}
+
