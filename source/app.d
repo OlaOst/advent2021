c4c09b2ad06e15d343025adb516ee030730389d0
Ola <ola@superoffice.com>
2021-12-01 13:44:57 +0100

    Day 1 part 2

--- source/app.d
+++ source/app.d
@@ -4,6 +4,7 @@ import std;
 void main()
 {
 	"day1input.txt".File.byLine.map!(line => line.to!int).array.day1part1.writeln;
+  "day1input.txt".File.byLine.map!(line => line.to!int).array.day1part2.writeln;
 }
 
 unittest
@@ -16,3 +17,13 @@ auto day1part1(int[] input)
   return input.zip(input[1..$]).count!(i => i[0] < i[1]);
 }
 
+unittest
+{
+  auto heights = [199,200,208,210,200,207,240,269,260,263];
+  assert(heights.day1part2 == 5);
+}
+auto day1part2(int[] input)
+{
+  auto windows = input.slide(3).map!(w => w.sum).array;
+  return windows.day1part1;
+}
