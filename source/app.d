a1de82054b935516490dd5d40c8f2075b94608de
OlaOst <olaa81@gmail.com>
2021-12-03 14:57:48 +0100

    Day 2 part 1

--- source/app.d
+++ source/app.d
@@ -6,6 +6,9 @@ void main()
 	auto day1input = "day1input.txt".slurp!int("%s").array;
   day1input.day1part1.writeln;
   day1input.day1part2.writeln;
+  
+  auto day2input = "day2input.txt".slurp!(string, int)("%s %s").array;
+  day2input.day2part1.writeln;
 }
 
 unittest
@@ -26,4 +29,36 @@ unittest
 auto day1part2(Range)(Range input)
 {
   return input.slide(3).map!sum.day1part1;
+}
+
+unittest
+{
+  auto text = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+  auto input = text.split("\n").map!(line => line.split(" ")).map!(line => tuple(line[0], line[1].to!int));
+  
+  assert(input.day2part1 == 150);
+}
+auto day2part1(Range)(Range input)
+{
+  auto horizontal = 0;
+  auto depth = 0;
+
+  // assume sub can fly, do not clamp depth above 0
+  foreach (line; input)
+  {
+    if (line[0] == "forward")
+      horizontal += line[1];
+    if (line[0] == "down")
+      depth += line[1];
+    if (line[0] == "up")
+      depth -= line[1];
+  }    
+      
+  return horizontal * depth;
 }
